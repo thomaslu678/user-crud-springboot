dc21fd0cfc7f60da22a704fafca3d3ad947c9fbc
thomaslu678 <thomaslu678@gmail.com>
2024-08-14 16:22:32 -0600

    populate User model

--- src/main/java/com/foodfinder/models/User.java
+++ src/main/java/com/foodfinder/models/User.java
@@ -1,4 +1,40 @@
 package com.foodfinder.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min=3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min=10, max = 10)
+    private String phone;
+
+    private LocalDateTime regDateAndTime;
+
 }
