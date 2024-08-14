84a0ef54bbfdc31323886308c29e1d08cf3bf697
thomaslu678 <thomaslu678@gmail.com>
2024-08-14 17:28:46 -0600

    populate controller

--- src/main/java/com/foodfinder/controllers/UserController.java
+++ src/main/java/com/foodfinder/controllers/UserController.java
@@ -1,4 +1,46 @@
 package com.foodfinder.controllers;
 
+import com.foodfinder.dtos.ApiResponseDto;
+import com.foodfinder.dtos.UserDetailsRequestDto;
+import com.foodfinder.exceptions.UserAlreadyExistsException;
+import com.foodfinder.exceptions.UserNotFoundException;
+import com.foodfinder.exceptions.UserServiceLogicException;
+import com.foodfinder.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    public UserService userService;
+
+    @PostMapping("/new")
+    public ResponseEntity<ApiResponseDto<?>> registerUser(@Valid @RequestBody UserDetailsRequestDto userDetailsRequestDto)
+            throws UserAlreadyExistsException, UserServiceLogicException {
+        return userService.registerUser(userDetailsRequestDto);
+    }
+
+    @GetMapping("/get/all")
+    public ResponseEntity<ApiResponseDto<?>> getAllUsers()
+            throws UserServiceLogicException {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiResponseDto<?>> updateUser(@Valid @RequestBody UserDetailsRequestDto userDetailsRequestDto,
+                                                        @PathVariable int id)
+            throws UserNotFoundException, UserServiceLogicException {
+        return userService.updateUser(userDetailsRequestDto, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponseDto<?>> deleteUser(@PathVariable int id)
+            throws UserNotFoundException, UserServiceLogicException {
+        return userService.deleteUser(id);
+    }
+
 }
