b40273d2cc0f458a8a5be547187de0935605b230
thomaslu678 <thomaslu678@gmail.com>
2024-08-14 17:33:57 -0600

    populate exception handler

--- src/main/java/com/foodfinder/handlers/UserServiceExceptionHandler.java
+++ src/main/java/com/foodfinder/handlers/UserServiceExceptionHandler.java
@@ -1,4 +1,46 @@
 package com.foodfinder.handlers;
 
+import com.foodfinder.dtos.ApiResponseDto;
+import com.foodfinder.dtos.ApiResponseStatus;
+import com.foodfinder.exceptions.UserAlreadyExistsException;
+import com.foodfinder.exceptions.UserNotFoundException;
+import com.foodfinder.exceptions.UserServiceLogicException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
 public class UserServiceExceptionHandler {
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<ApiResponseDto<?>> UserNotFoundExceptionHandler(UserNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponseDto<>(ApiResponseStatus.FAIL.name(), exception.getMessage()));
+    }
+
+    @ExceptionHandler(value = UserAlreadyExistsException.class)
+    public ResponseEntity<ApiResponseDto<?>> UserAlreadyExistsExceptionHandler(UserAlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponseDto<>(ApiResponseStatus.FAIL.name(), exception.getMessage()));
+    }
+
+    @ExceptionHandler(value = UserServiceLogicException.class)
+    public ResponseEntity<ApiResponseDto<?>> UserServiceLogicExceptionHandler(UserServiceLogicException exception) {
+        return ResponseEntity.badRequest().body(new ApiResponseDto<>(ApiResponseStatus.FAIL.name(), exception.getMessage()));
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseDto<?>> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception) {
+
+        List<String> errorMessage = new ArrayList<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMessage.add(error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(new ApiResponseDto<>(ApiResponseStatus.FAIL.name(), errorMessage.toString()));
+    }
+
 }
