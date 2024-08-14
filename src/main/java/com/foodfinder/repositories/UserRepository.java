8f05c5832d221291b66f8d59d324d175f785cb47
thomaslu678 <thomaslu678@gmail.com>
2024-08-14 16:28:27 -0600

    populate repository class

--- src/main/java/com/foodfinder/repositories/UserRepository.java
+++ src/main/java/com/foodfinder/repositories/UserRepository.java
@@ -1,4 +1,20 @@
 package com.foodfinder.repositories;
 
-public interface UserRepository {
+import com.foodfinder.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    // Developers can define methods in repository interfaces with custom query keywords,
+    // and Spring Data JPA automatically translates them into appropriate SQL queries.
+    User findByEmail(String email);
+
+    User findByUsername(String userName);
+
+    List<User> findAllByOrderByRegDateTimeDesc();
+
 }
