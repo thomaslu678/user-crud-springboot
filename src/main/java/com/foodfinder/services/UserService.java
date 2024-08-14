3963f50b6e4e0fdd9149a08e402fc385f53a0b1c
thomaslu678 <thomaslu678@gmail.com>
2024-08-14 17:23:00 -0600

    create UserService interface

--- src/main/java/com/foodfinder/services/UserService.java
+++ src/main/java/com/foodfinder/services/UserService.java
@@ -1,4 +1,26 @@
 package com.foodfinder.services;
 
+import com.foodfinder.dtos.ApiResponseDto;
+import com.foodfinder.dtos.UserDetailsRequestDto;
+import com.foodfinder.exceptions.UserAlreadyExistsException;
+import com.foodfinder.exceptions.UserNotFoundException;
+import com.foodfinder.exceptions.UserServiceLogicException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService {
+
+    ResponseEntity<ApiResponseDto<?>> registerUser(UserDetailsRequestDto newUserDetails)
+            throws UserAlreadyExistsException, UserServiceLogicException;
+
+    ResponseEntity<ApiResponseDto<?>> getAllUsers()
+            throws UserServiceLogicException;
+
+    ResponseEntity<ApiResponseDto<?>> updateUser(UserDetailsRequestDto newUserDetails, int id)
+            throws UserNotFoundException, UserServiceLogicException;
+
+    ResponseEntity<ApiResponseDto<?>> deleteUser(int id)
+            throws UserServiceLogicException, UserNotFoundException;
+
 }
