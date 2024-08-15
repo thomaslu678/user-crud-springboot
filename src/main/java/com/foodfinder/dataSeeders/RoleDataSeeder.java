54cbfcbd9990bac9ac7d9ac98de84295d141709d
thomaslu678 <thomaslu678@gmail.com>
2024-08-15 17:25:41 -0600

    Create RoleDataSeeder.java

---
+++ src/main/java/com/foodfinder/dataSeeders/RoleDataSeeder.java
@@ -0,0 +1,34 @@
+package com.foodfinder.dataSeeders;
+
+import com.foodfinder.models.ERole;
+import com.foodfinder.models.Role;
+import com.foodfinder.repositories.RoleRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class RoleDataSeeder {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @EventListener
+    @Transactional
+    public void LoadRoles(ContextRefreshedEvent event) {
+
+        List<ERole> roles = Arrays.stream(ERole.values()).toList();
+
+        for(ERole erole: roles) {
+            if (roleRepository.findByName(erole)==null) {
+                roleRepository.save(new Role(erole));
+            }
+        }
+
+    }
+
+}
