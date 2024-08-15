cec5434405bccb03165d997eddd6271c1f157b49
thomaslu678 <thomaslu678@gmail.com>
2024-08-15 17:36:05 -0600

    rename handler + RoleExceptionHandler

---
+++ src/main/java/com/foodfinder/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,89 @@
+package com.foodfinder.handlers;
+
+import com.foodfinder.dtos.ApiResponseDto;
+import com.foodfinder.dtos.ApiResponseStatus;
+import com.foodfinder.exceptions.RoleNotFoundException;
+import com.foodfinder.exceptions.UserAlreadyExistsException;
+import com.foodfinder.exceptions.UserNotFoundException;
+import com.foodfinder.exceptions.UserServiceLogicException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<ApiResponseDto<?>> UserNotFoundExceptionHandler(UserNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(
+                        ApiResponseDto.builder()
+                                .isSuccess(false)
+                                .message(exception.getMessage())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value = UserAlreadyExistsException.class)
+    public ResponseEntity<ApiResponseDto<?>> UserAlreadyExistsExceptionHandler(UserAlreadyExistsException exception) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(
+                        ApiResponseDto.builder()
+                                .isSuccess(false)
+                                .message(exception.getMessage())
+                                .build()
+                );    }
+
+    @ExceptionHandler(value = UserServiceLogicException.class)
+    public ResponseEntity<ApiResponseDto<?>> UserServiceLogicExceptionHandler(UserServiceLogicException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        ApiResponseDto.builder()
+                                .isSuccess(false)
+                                .message(exception.getMessage())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value = RoleNotFoundException.class)
+    public ResponseEntity<ApiResponseDto<?>> RoleNotFoundExceptionHandler(RoleNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(
+                        ApiResponseDto.builder()
+                                .isSuccess(false)
+                                .message(exception.getMessage())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseDto<?>> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception) {
+
+        List<String> errorMessage = new ArrayList<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMessage.add(error.getDefaultMessage());
+        });
+
+        return ResponseEntity
+                .badRequest()
+                .body(
+                        ApiResponseDto.builder()
+                                .isSuccess(false)
+                                .message("Registration Failed: Please provide valid data.")
+                                .response(errorMessage)
+                                .build()
+                );
+
+    }
+
+}
