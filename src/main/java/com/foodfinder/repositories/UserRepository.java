c8c4b383142c2382741edbe7ad1c389f8313a298
thomaslu678 <thomaslu678@gmail.com>
2024-08-15 15:36:29 -0600

    Update UserRepository.java

--- src/main/java/com/foodfinder/repositories/UserRepository.java
+++ src/main/java/com/foodfinder/repositories/UserRepository.java
@@ -5,13 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     // Developers can define methods in repository interfaces with custom query keywords,
     // and Spring Data JPA automatically translates them into appropriate SQL queries.
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 
     User findByUsername(String userName);
 
