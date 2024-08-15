617ddb5c2508802ef622c4c62118b5a1055a8f50
thomaslu678 <thomaslu678@gmail.com>
2024-08-15 15:37:42 -0600

    Create TestController.java

---
+++ src/main/java/com/foodfinder/controllers/TestController.java
@@ -0,0 +1,22 @@
+package com.foodfinder.controllers;
+
+import com.foodfinder.dtos.ApiResponseDto;
+import com.foodfinder.dtos.ApiResponseStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping("/")
+    public ResponseEntity<ApiResponseDto<?>> Test() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ApiResponseDto<>( "Let's learn spring security with JWT!", ApiResponseStatus.SUCCESS));
+    }
+}
