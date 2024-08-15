239b5543e0e2a75aa9801df11b96f311779b8879
thomaslu678 <thomaslu678@gmail.com>
2024-08-15 15:37:39 -0600

    @Builder in ApiResponseDto

--- src/main/java/com/foodfinder/dtos/ApiResponseDto.java
+++ src/main/java/com/foodfinder/dtos/ApiResponseDto.java
@@ -1,9 +1,11 @@
 package com.foodfinder.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class ApiResponseDto<T> {
     private String status;
