db38aa7665624a265f4b4e23e65b8429fdc5560a
thomaslu678 <thomaslu678@gmail.com>
2024-08-18 14:29:31 -0600

    add @GetMapping /signup + html

---
+++ src/main/java/com/foodfinder/controllers/FormController.java
@@ -0,0 +1,18 @@
+package com.foodfinder.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/api/auth")
+public class FormController {
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+}
