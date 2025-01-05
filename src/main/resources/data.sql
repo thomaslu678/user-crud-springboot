2793ccc7fe671acf95eb073d04ecd7bb82f2893a
thomaslu678 <thomaslu678@gmail.com>
2025-01-05 09:06:21 -0700

    initial values in data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CUSTOM_USER (email, enabled, username, password) VALUES ('david@david.com', true, 'david', 'daviddavid');
+INSERT INTO CUSTOM_USER (email, enabled, username, password) VALUES ('john@john.com', true, 'john', 'johnjohn');
+INSERT INTO CUSTOM_USER (email, enabled, username, password) VALUES ('sam@sam.com', true, 'sam', 'samsamsamsam');
+INSERT INTO CUSTOM_USER (email, enabled, username, password) VALUES ('james@abc.com', true, 'james', 'jamesjames');
